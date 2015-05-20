9383ebaa8129330f5c722b09ad564ad00983f4a4
Geonmo <ry840901@gmail.com>
2015-05-20 08:03:24 +0200

    Add rechit efficiency plots.

--- Validation/MuonGEMRecHits/python/MuonGEMRecHits_cfi.py
+++ Validation/MuonGEMRecHits/python/MuonGEMRecHits_cfi.py
@@ -12,3 +12,20 @@ gemRecHitsValidation = cms.EDAnalyzer('GEMRecHitsValidation',
     nBinGlobalXY = cms.untracked.int32(720), 
 )
 
+gemRecHitTrackValidation = cms.EDAnalyzer('GEMRecHitTrackMatch',
+  simInputLabel = cms.untracked.string('g4SimHits'),
+  simTrackCollection = cms.InputTag('g4SimHits'),
+  simVertexCollection = cms.InputTag('g4SimHits'),
+  verboseSimHit = cms.untracked.int32(0),
+  # GEM RecHit matching:
+  verboseGEMDigi = cms.untracked.int32(0),
+  gemRecHitInput = cms.InputTag("gemRecHits"),
+  minBXGEM = cms.untracked.int32(-1),
+  maxBXGEM = cms.untracked.int32(1),
+  matchDeltaStripGEM = cms.untracked.int32(1),
+  gemMinPt = cms.untracked.double(5.0),
+  gemMinEta = cms.untracked.double(1.55),
+  gemMaxEta = cms.untracked.double(2.45),
+)
+
+gemRecHitValidation = cms.Sequence( gemRecHitsValidation+gemRecHitTrackValidation )
